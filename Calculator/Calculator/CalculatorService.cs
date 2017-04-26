a3c54d15c6c3766d860053dbe7f91d8b9e0fc818
PuranCJoshi <pjoshi@tavisca.com>
2017-04-26 10:50:41 +0530

    Added Calculator project

---
+++ Calculator/Calculator/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class CalculatorService : ICalculatorService
+    {
+        public string Add(string text1, string text2)
+        {
+            return text1 + text2;
+        }
+
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Calculator/Calculator/ICalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calculator
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract(Name ="AddInteger")]
+        int Add(int num1, int num2);
+
+        [OperationContract(Name = "AddFloat")]
+        float Add(float num1, float num2);
+
+        [OperationContract(Name = "AddString")]
+        string Add(string text1, string text2);
+    }
+}
