6ef7ce25e41c54cccda5d3593aea3a3ff22584eb
PuranCJoshi <pjoshi@tavisca.com>
2017-04-26 14:49:16 +0530

    Code cleanup and refactor

--- Calculator/Calculator/CalculatorService.cs
+++ Calculator/Calculator/CalculatorService.cs
@@ -10,30 +10,9 @@ namespace Calculator
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class CalculatorService : ICalculatorService
     {
-        public string Add(string text1, string text2)
-        {
-            return text1 + text2;
-        }
-
-        public float Add(float num1, float num2)
-        {
-            return num1 + num2;
-        }
-
         public int Add(int num1, int num2)
         {
             return num1 + num2;
-        }
-
-        public User Get(User user)
-        {
-            switch (user.EmpId)
-            {
-                case 823:
-                    return new User { Name = "Puran Joshi", EmpId = 823, Date_Of_Birth = new DateTime(1988, 07, 30) };
-                default:
-                    throw new Exception("User doesn't exists");
-            }
         }
     }
 }
--- Calculator/Calculator/ICalculatorService.cs
+++ Calculator/Calculator/ICalculatorService.cs
@@ -7,30 +7,10 @@ using System.Text;
 
 namespace Calculator
 {
-    [ServiceContract(Namespace ="CalculatorSvc")]
+    [ServiceContract]
     public interface ICalculatorService
     {
-        [OperationContract(Name ="AddInteger")]
+        [OperationContract(Name = "AddInteger")]
         int Add(int num1, int num2);
-
-        [OperationContract(Name = "AddFloat")]
-        float Add(float num1, float num2);
-
-        [OperationContract(Name = "AddString")]
-        string Add(string text1, string text2);
-
-        [OperationContract(Name = "GetUserInfo")]
-        User Get(User user);
-    }
-
-    [DataContract]
-    public class User
-    {
-        [DataMember]
-        public string Name { get; set; }
-        [DataMember]
-        public int EmpId { get; set; }
-        [DataMember]
-        public DateTime Date_Of_Birth { get; set; }
     }
 }
