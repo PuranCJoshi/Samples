ba017b4cd7c6e252025fb2cff54a7648263f99c1
PuranCJoshi <pjoshi@tavisca.com>
2017-04-26 14:20:26 +0530

    Added a test acse

---
+++ Calculator/Calculator/Test/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public void AddNumber()
+        {
+            var calculatorSvc = new CalculatorService();
+            int result = calculatorSvc.Add(1, 2);
+            Console.WriteLine("Test Running");
+        }
+    }
+}
